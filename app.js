1cbf8baf998a11686c57e351861c18e98f7886fe
Rajendra <rchoudhary157@gmail.com>
2021-01-21 07:11:24 +0530

    Changes 1

--- app.js
+++ app.js
@@ -16,13 +16,3 @@ const newSlide = () => {
     });
 }
 newSlide();
-
-var progressbar = document.getElementById('progress');
-var percent = document.getElementById('percent');
-
-var totalHeight = document.body.scrollHeight - window.innerHeight;
-window.onscroll = function() {
-    var progress = (window.pageYOffset / totalHeight) * 100;
-    progressbar.style.height = progress + "%";
-    percent.innerHTML = "Page Scrolled " + Math.round(progress) + "%"
-}
